95817c54e8f3de31b7f972c344a9bf2116be2b85
Hansen <thomas.hansen@yrbrands.com>
2016-04-13 13:51:52 +0200

    first commit

---
+++ app/components/image-input.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  file: null,
+  actions: {
+    fileSelectionChanged: function(file) {
+      this.set('file', file)
+    },
+  },
+});
